a3d27eeaf569990aee4d6aa27fc256a5bb8c5a46
pasha2305 <pasha.k2305@gmail.com>
2021-07-03 20:58:58 +0300

    optimize request

--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
- 
+
+DRIVER_PATH = '/home/admin/Документы/parser/geckodriver'
+
 class Product_parser(object):
 
     URL1 = 'https://5ka.ru/special_offers/'
@@ -13,34 +15,29 @@ class Product_parser(object):
         self.driver.find_element_by_class_name('search_input').send_keys(self.product_name)
         self.driver.find_element_by_class_name('search_btn').click()
         list_items = self.driver.find_elements_by_class_name('sale-card')
-        print(list_items)
-
-
-
+        for item in list_items:
+            print(item.find_element_by_css_selector('p.sale-card__title').text)
 
 def main():
-    driver = webdriver.Firefox(executable_path='/home/admin/Документы/parser/geckodriver')
+    driver = webdriver.Firefox(executable_path=DRIVER_PATH)
     input_number = -1
     while input_number != 0:
         print_menu()
-        input_number = int(input("Enter your choice here:"))
+        input_number = int(input("Ввод:"))
         if input_number == 1: 
-            product = input("Enter product name:")
+            product = input("Введите название продукта:")
             parser = Product_parser(driver, product)
             parser.parse_product()
         elif input_number == 2:
             print('\nВыберите интересующий магазин:\n' +
             '1. Пятерочка\n'+
             '2. Карусель\n'+
-            '3. Перекресток\n')
+            '3. Перекресток\n')      
             shop = input('Введите номер:')
-            ##parser1 = Parser(driver, shop)
-            ##parser1.shop()
         elif input_number != 0:
             print('Указан некорректный номер.')
-  
-    driver.close()
-    driver.quit()
+            driver.close()
+            driver.quit()
 
 def print_menu():
     print('1. Find element by name')
