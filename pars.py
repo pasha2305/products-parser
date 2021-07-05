2e94fdc5058796375bc2a1e61bc1db8315a5d58c
pasha2305 <68544809+pasha2305@users.noreply.github.com>
2021-07-05 15:57:12 +0300

    modified module in class

---
+++ pars.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import time
+from product_pars import Product_pars
+
+DRIVER_PATH = '/Users/admin/Desktop/products-parser/macos_driver/geckodriver'
+
+def main():
+    driver = webdriver.Firefox(executable_path=DRIVER_PATH)
+    input_number = -1
+    while input_number != 0:
+        print_menu()
+        input_number = int(input("Ввод:"))
+        if input_number == 1: 
+            product = input("Введите название продукта:")
+            parser = Product_pars(driver, product)
+            parser.parse_product_5ka()
+        elif input_number == 2:
+            print('\nВыберите интересующий магазин:\n' +
+            '1. Пятерочка\n'+
+            '2. Карусель\n'+
+            '3. Перекресток\n')      
+            shop = input('Введите номер:')
+        elif input_number != 0:
+            print('Указан некорректный номер.')
+            driver.close()
+            driver.quit()
+
+def print_menu():
+    print('1. Find element by name')
+    print('2. Download all special offers')
+    print('0. Quit')
+
+if __name__ == "__main__":
+    main()
